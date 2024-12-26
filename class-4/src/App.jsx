fe11dd1fb3942044d684a7cc27833dc809320e26
sarfaraztahirarain2006@gmail.com <areebatahirarain@gmail.com>
2024-12-27 00:13:17 +0500

    don!

---
+++ class-4/src/App.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+import "./App.css";
+import Count from "./components/count";
+
+function App() {
+  return (
+    <>
+      <Count name="Rabia" course="web" />
+    </>
+  );
+}
+
+export default App;
---
+++ class-4/src/components/count.jsx
@@ -0,0 +1,33 @@
+import React,{useEffect , useState} from 'react'
+
+const Count = ({ name, course }) => {
+    let [count, setCount] = useState(0)
+    let [userName, setUserName] = useState(name)
+
+    // console.log(count);
+    // console.log(setCount);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(json => console.log(json))
+
+    }, [userName])
+
+
+    function increment() {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            <p>{userName}</p>
+            <button onClick={() => { setUserName("Laiba") }}>change name</button>
+            <p> count: {count}</p>
+            <button onClick={increment}>increase</button>
+        </div>
+    );
+};
+
+
+export default Count
